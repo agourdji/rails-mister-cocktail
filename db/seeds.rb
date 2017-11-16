4929d5069f70defc2f6127a627fccc6a0a285e5f
Audrey Gourdji <agourdji@gmail.com>
2017-11-16 13:54:18 +0800

    Added models for ingredients, doses, and cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+# records = JSON.parse(File.read('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'))
+# records.each do |record|
+#   Ingredient.create!(name: record)
+# end
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "dry vermouth")
+Ingredient.create(name: "brandy")
+Ingredient.create(name: "coffee")
+Ingredient.create(name: "triple sec")
+Ingredient.create(name: "strawberry puree")
+Ingredient.create(name: "ginger beer")
+Ingredient.create(name: "matcha")
